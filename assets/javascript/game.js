dc8a77dc499d561e6b8ce4942477aff8823e39fc
Robert Bolton <therobshock@yahoo.com>
2019-02-28 22:35:46 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,45 @@
 // "Crystal Collector"
+
+$(document).ready(function () {
+  
+  var targetNum = 0;
+  var totalScore = 0;
+  var gameOver = true;
+
+
+  var numGen = function (max, min) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+
+    function gameStart () {
+      totalScore = 0;
+      targetNum = numGen(121, 19);
+      $("#target").text(targetNum);
+      $("#square1").attr("value", numGen(13, 1));
+      $("#square2").attr("value", numGen(13, 1));
+      $("#square3").attr("value", numGen(13, 1));
+      $("#square4").attr("value", numGen(13, 1));
+      gameOver = false;
+    }
+
+    if (gameOver && totalScore === targetNum || totalScore > targetNum) {
+      gameOver = true;
+      gameStart();
+
+    }
+
+
+    $(".click").on("click", function () {
+      totalScore += parseInt(this.value);
+        console.log(totalScore);        
+        
+    });
+
+
+
+
+
+})
+
+
