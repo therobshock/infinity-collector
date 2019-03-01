be054ed7aeab860844069366f076c54a14ab57a9
Robert Bolton <therobshock@yahoo.com>
2019-03-01 00:57:01 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,36 +4,50 @@ $(document).ready(function () {
   
   var targetNum = 0;
   var totalScore = 0;
-  var gameOver = true;
+  var totalWins = 0;
+  var totalLosses = 0;
 
 
   var numGen = function (max, min) {
       return Math.floor(Math.random() * (max - min)) + min;
     }
 
+    gameStart();
 
     function gameStart () {
       totalScore = 0;
       targetNum = numGen(121, 19);
       $("#target").text(targetNum);
+      $("#wins").text("Wins: " + totalWins);
+      $("#losses").text("Losses: " + totalLosses);
+      $("#score").text(totalScore);
       $("#square1").attr("value", numGen(13, 1));
       $("#square2").attr("value", numGen(13, 1));
       $("#square3").attr("value", numGen(13, 1));
       $("#square4").attr("value", numGen(13, 1));
-      gameOver = false;
     }
 
-    if (gameOver && totalScore === targetNum || totalScore > targetNum) {
-      gameOver = true;
-      gameStart();
-
-    }
+    
 
 
     $(".click").on("click", function () {
       totalScore += parseInt(this.value);
-        console.log(totalScore);        
+        console.log(totalScore);   
+      $("#score").text(totalScore);
+
         
+        if (totalScore === targetNum) {
+            totalWins++;
+        }
+    
+        if (totalScore > targetNum) {
+          totalLosses++;
+        }    
+
+        if (totalScore === targetNum || totalScore > targetNum) {
+          gameStart();    
+        }
+
     });
 
 
